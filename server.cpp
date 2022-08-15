9578be24bc02ddc60bca3f978c585518b5ab2c7e
42hena <somnium1108@gmail.com>
2022-08-15 17:26:34 +0900

    feat: check valid port number
    - if port number has non numeric number, get failure
    - if port number is well-known port number or dynamic port number or
      not range in unsigned short, get failure

--- server.cpp
+++ server.cpp
@@ -46,10 +46,80 @@ Server::~Server(void)
 	return ;
 }
 
+#define WELL_KNOWN	0
+#define REGISTERED	1
+#define DYNAMIC		2
+#define ERROR		3
+int getPortType(int value)
+{
+	if (value >= 0 && value < 1024)
+		return (WELL_KNOWN);
+	else if (value < 49152)
+		return (REGISTERED);
+	else if (value < 65536) // unsigned short면 왜 에러를??
+		return (DYNAMIC);
+	return (ERROR);
+}
 
+bool getPortNumber(const char *str, int *value)
+{
+	long long total = 0;
+
+	while (*str)
+	{
+		total = total * 10 + *str - '0';
+		#ifdef DEBUG
+		std::cout << "total:" << total << '\n';
+		#endif
+		// if (total > 0xcffffff || total < 0xffffffff)
+		if (total > 2147483647 || total < -2147483648)
+			return (false);
+		str++;
+	}
+
+	// getType: WELL_KNOWN | REGISTERED | DYNAMIC | ERROR
+	if (getPortType(total) != REGISTERED)
+		return (false);
+	*value = total;
+	return (true);
+}
+
+// true: valid port | false: not valid port
 bool	Server::is_valid_port(std::string port)
 {
-	return (port.find_first_not_of("0123456789") == std::string::npos);
+	bool	flag;
+	int		retPort;
+
+	flag = ( port.find_first_not_of("0123456789") == std::string::npos );
+
+	#ifdef DEBUG
+	std::cout << "find_first_not_of start\n";
+	std::cout << "Flag Value: " << flag << '\n';
+	std::cout << "----------END----------\n\n";
+	#endif
+
+	if (!flag)
+		return (flag);
+	
+	#ifdef DEBUG
+	std::cout << "getPortNumber start\n";
+	#endif
+
+	flag = getPortNumber(port.c_str(), &retPort);
+	if (!flag)
+		return (flag);
+	
+	#ifdef DEBUG
+	std::cout << "----------END----------\n\n";
+	#endif
+
+	s_port_ = static_cast<unsigned short>(retPort);
+
+	#ifdef DEBUG
+	std::cout << "real portnum: " << s_port_ << '\n';
+	#endif
+
+	return (flag);
 }
 
 bool	Server::is_valid_pwd(std::string pwd)
